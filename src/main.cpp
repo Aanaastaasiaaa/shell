2b20894db7b5114fd087c8f5ddd1ba8579a7ad28
anastasia <bolobanav6203214@yandex.ru>
2025-11-22 00:04:16 +0300

    7?

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <sstream>
+#include <cstdlib> // для getenv()
 
 using namespace std;
 
@@ -10,22 +12,35 @@ int main()
     vector<string> history;
     string input;
     bool running = true;
- string history_file = "kubsh_history.txt";
-ofstream write_file(history_file, ios::app);
+    string history_file = "kubsh_history.txt";
+    
+    // Открываем файл для записи в режиме добавления
+    ofstream write_file(history_file, ios::app);
+    
+    if (!write_file.is_open()) {
+        cerr << "Error: Could not open history file!" << endl;
+        return 1;
+    }
+
     while (running && getline(cin, input)) 
     {
         if (input.empty()) {
             continue;
         }
+        
+        // Записываем команду в историю файла
         write_file << input << endl;
-        write_file.flush(); 
+        write_file.flush();
+        
+        // Добавляем команду в историю памяти
+        history.push_back(input);
+        
         if (input == "\\q") 
         {
             running = false;
             break;
         }
-       else if (input.find("debug ") == 0) 
-        {
+        else if (input.find("debug ") == 0) {
             string text = input.substr(6);
             if (text.size() >= 2)
             {
@@ -37,15 +52,32 @@ ofstream write_file(history_file, ios::app);
                 }
             }
             cout << text << endl;
-            history.push_back(input);
+        }
+        else if (input.substr(0,4) == "\\e $") {
+            string var_name = input.substr(4);
+            const char* env_value = getenv(var_name.c_str());
+            
+            if (env_value != nullptr) {
+                string value(env_value);
+                if (value.find(':') != string::npos) {
+                    stringstream ss(value);
+                    string item;
+                    while (getline(ss, item, ':')) {
+                        cout << item << endl;
+                    }
+                } else {
+                    cout << value << endl;
+                }
+            } else {
+                cout << "Environment variable '" << var_name << "' not found" << endl;
+            }
         }
         else
         {
             cout << input << ": command not found" << endl;
-            history.push_back(input);
         }
- write_file.close();
     }
+    
+    write_file.close();
     return 0;
 }
-
