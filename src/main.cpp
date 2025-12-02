6c9a1c81ca4b0a1bc98146589a19ec5aa44ecc64
anastasia <bolobanav6203214@yandex.ru>
2025-12-02 22:54:14 +0300

    1-8

--- src/main.cpp
+++ src/main.cpp
@@ -3,61 +3,36 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
-#include <cstdlib> // для getenv()
+#include <cstdlib>
 
 using namespace std;
 
-int main()
+int main() 
 {
     vector<string> history;
     string input;
     bool running = true;
-<<<<<<< HEAD
- string history_file = "kubsh_history.txt";
-ofstream write_file(history_file, ios::app);
-=======
     string history_file = "kubsh_history.txt";
-    
-    // Открываем файл для записи в режиме добавления
     ofstream write_file(history_file, ios::app);
     
-    if (!write_file.is_open()) {
-        cerr << "Error: Could not open history file!" << endl;
-        return 1;
-    }
-
->>>>>>> 2b20894db7b5114fd087c8f5ddd1ba8579a7ad28
     while (running && getline(cin, input)) 
     {
         if (input.empty()) {
             continue;
         }
-<<<<<<< HEAD
-        write_file << input << endl;
-        write_file.flush(); 
-=======
-        
-        // Записываем команду в историю файла
         write_file << input << endl;
         write_file.flush();
         
-        // Добавляем команду в историю памяти
-        history.push_back(input);
-        
->>>>>>> 2b20894db7b5114fd087c8f5ddd1ba8579a7ad28
         if (input == "\\q") 
         {
             running = false;
             break;
         }
-<<<<<<< HEAD
-       else if (input.find("debug ") == 0) 
+        else if (input.find("debug ") == 0) 
         {
-=======
-        else if (input.find("debug ") == 0) {
->>>>>>> 2b20894db7b5114fd087c8f5ddd1ba8579a7ad28
             string text = input.substr(6);
-            if (text.size() >= 2)
+            
+            if (text.size() >= 2) 
             {
                 char first = text[0];
                 char last = text[text.size()-1];
@@ -66,9 +41,12 @@ ofstream write_file(history_file, ios::app);
                     text = text.substr(1, text.size()-2);
                 }
             }
+            
             cout << text << endl;
+            history.push_back(input);
         }
-        else if (input.substr(0,4) == "\\e $") {
+        else if (input.substr(0,4) == "\\e $") 
+        {
             string var_name = input.substr(4);
             const char* env_value = getenv(var_name.c_str());
             
@@ -87,15 +65,13 @@ ofstream write_file(history_file, ios::app);
                 cout << "Environment variable '" << var_name << "' not found" << endl;
             }
         }
-        else
+        else 
         {
             cout << input << ": command not found" << endl;
+            history.push_back(input);
         }
- write_file.close();
     }
     
     write_file.close();
     return 0;
 }
-
-
